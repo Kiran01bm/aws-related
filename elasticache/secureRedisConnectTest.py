a93ab2277d5ee6a792857e11f35ebe3ffb2ad93e
kiran01bm <kiran01bm@gmail.com>
2020-03-19 15:27:00 +1100

    + virtualenv example for lambdas with custom python packages

--- elasticache/secureRedisConnectTest.py
+++ elasticache/secureRedisConnectTest.py
@@ -17,3 +17,15 @@ python get-pip.py --user
 export PATH=$PATH:/home/myself/.local/bin
 pip install redis
 """
+
+"""
+Deploying Lambda with Custom Packge Installs
+
+sudo pip install virtualenv
+virtualenv -p /usr/bin/python redistest
+pip install redis
+vi lambda_function.py
+mv lambda_function.py redistest/lib/python2.7/site-packages
+cd redistest/lib/python2.7/site-packages
+zip -r9 lambda_function.zip *
+"""
